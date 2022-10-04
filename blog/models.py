b2090736a5f09421c6ba9d02c07bcb3c52428568
tkddbs369369 <tkddbs369369@naver.com>
2022-10-04 14:27:23 +0900

    9/26

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,8 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
 
-    created_at = models.DateTimeField()
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'[{self.pk}] {self.title}'
