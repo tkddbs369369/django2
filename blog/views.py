acc60f5640621cb15816959f7c9fd166d1f0647e
tkddbs369369 <tkddbs369369@naver.com>
2022-10-04 15:37:22 +0900

    9/26

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .models import Post
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', {'posts': posts})
+
 # Create your views here.
